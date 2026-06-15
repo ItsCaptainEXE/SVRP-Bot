742297784fb9c2888673baaeb014073a87590eee
CaptainEXE <189309636+ItsCaptainEXE@users.noreply.github.com>
2026-06-15 09:03:05 -0600

    Update main.py

--- main.py
+++ main.py
@@ -31,7 +31,7 @@ intents = discord.Intents.default()
 intents.message_content = True
 intents.members = True
 
-bot = commands.Bot(command_prefix="!", intents=intents)
+bot = commands.Bot(command_prefix="$", intents=intents)
 
 # Load Cogs
 async def load_extensions():
